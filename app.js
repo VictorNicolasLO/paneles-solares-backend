bcb052cd0be5583c319c9fe5ab954e29e6ecefa9
VictorNicolasLO <victornicolaslo@hotmail.com>
2018-12-02 22:24:16 -0700

    db

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const config = require('./config');
@@ -19,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use('/api/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
