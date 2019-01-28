bc7972d403ab41fcd0d259aff0469a66505ee604
Adriana Ochoa <suadocca@gmail.com>
2019-01-28 15:08:43 -0700

    index modified
    added solarPanel routes path and its api

--- modules/index.js
+++ modules/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const auth = require('./auth/auth.routes');
 const solarHeater = require('./solar-heater/solar-heater.routes');
+const solarPanel = require('./solar-panel/solar-panel.routes');
 const api = express.Router();
 
 api.use('/auth', auth);
 api.use('/solar-heater', solarHeater);
+api.use('/solar-panel', solarPanel);
 
 module.exports = api;
